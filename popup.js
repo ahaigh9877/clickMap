e62cd8ef595a7aa05ed672508a69364861c08c75
Alastair Haigh <a.haigh@skimgroup.com>
2022-08-25 17:07:37 +0200

    add markers

--- popup.js
+++ popup.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
 
-    $('.lightning').on('click',function(e){
+    $('.marker').on('click',function(e){
         e.stopPropagation();
-        $(e.target).siblings('.popup').show()
+        $('.popup').hide('fast')
+        $(e.target).siblings('.popup').show('fast')
     })
 
     $('.popup').on('click',function(e){
         e.stopPropagation();
     })
     $('.close').on('click',function(e){
-        $(e.target).closest('.popup').hide()
+        $(e.target).closest('.popup').hide('fast')
     })
     $('.map').on('click',function(){
-        $('.popup').hide()
+        $('.popup').hide('fast')
     })
 })
